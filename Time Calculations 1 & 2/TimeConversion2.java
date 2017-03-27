f45bcd91b59839787d338c6244bc196f26e3c7c6
Owner <Owner@192.168.1.107>
2017-03-26 23:38:10 -0400

    Time COnversion Assignments 1 & 2

---
+++ Time Calculations 1 & 2/TimeConversion2.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class TimeConversion2 {
+
+	public static void main(String[] args) 
+	{
+		Scanner input = new Scanner(System.in);
+	 	System.out.print("Please enter a time in decimal format: "); 
+	 	double time = input.nextDouble(); 
+	 	double newtime = time*60; 
+	 	double mins = newtime % 60; 
+		 double hours = (newtime - mins)/60; 
+		 input.close(); 
+		 	System.out.printf("User entered time of " + time + " is %.0f hours and %.0f mins", hours, mins ); 
+
+			
+
+	}
+
+}
